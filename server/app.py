0213f88c081cf8b3153b1f9e9c8dccc90e196448
tejaswi98 <tejaswi98@gmail.com>
2019-10-19 01:46:45 -0500

    Updated enpoint with CORS enabled
    CORS enabled on all routes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/getInterviewSchedule', methods=['POST'])
 def interviewScheduler():
     #file = request.files['StudentInput_Example']
     files = request.files.getlist('files[]')
+    print(files)
     return files
 
 @app.route('/getOfferMatches')
