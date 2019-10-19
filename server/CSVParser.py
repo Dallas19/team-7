6169147f3485c7ade513bf86ae4e0a26435dba62
joshuadargan <joshuadargan282@ufl.edu>
2019-10-19 02:14:05 -0400

    CSV Parser

---
+++ server/CSVParser.py
@@ -0,0 +1,55 @@
+# importing csv module
+import csv
+import os
+from builtins import staticmethod
+
+
+class CSVParser:
+
+    #Input:     path\\filename
+    #Output:    2D list of strings representing the rows from the student CSV.
+    @staticmethod
+    def parseStudentFile(filename):
+        # initializing the titles and rows list
+        fields = []
+        rows = []
+
+        cur_path = os.path.dirname(__file__)
+        new_path = os.path.relpath(filename, cur_path)
+        # reading csv file
+        with os.open(new_path, 'r') as csvfile:
+            # creating a csv reader object
+            csvreader = csv.reader(csvfile)
+
+            # extracting field names through first row
+            fields = csvreader.__next__()
+
+            # extracting each data row one by one
+            for row in csvreader:
+                rows.append(row)
+        return rows
+
+    @staticmethod
+    #Input:     path\\filename (CSV file)
+    #Output:    2D list of strings representing the rows from the CSV.
+    def parseCompanyFile(filename):
+        # initializing the titles and rows list
+        fields = []
+        rows = []
+
+        cur_path = os.path.dirname(__file__)
+        new_path = os.path.relpath(filename, cur_path)
+
+
+        # reading csv file
+        with os.open(new_path, 'r') as csvfile:
+            # creating a csv reader object
+            csvreader = csv.reader(csvfile)
+
+            # extracting field names through first row
+            fields = csvreader.__next__()
+
+            # extracting each data row one by one
+            for row in csvreader:
+                rows.append(row)
+        return rows
