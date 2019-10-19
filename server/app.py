5c1a624071c4567f5ad539352a4c0e557d881124
tejaswi98 <tejaswi98@gmail.com>
2019-10-19 01:25:16 -0500

    Updated getInterviewSchedule
    GetInterviewSchedule endpoint can accept multiple files

--- server/app.py
+++ server/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/getInterviewSchedule')
+@app.route('/getInterviewSchedule', methods=['POST'])
 def interviewScheduler():
-  return 'interviewScheduler'
+    #file = request.files['StudentInput_Example']
+    files = request.files.getlist('files[]')
+    return files
 
 @app.route('/getOfferMatches')
 def offerMatches():
   return 'OfferMatches'
+
+
+class File:
+  def __init__(self, name):
+    self.name = name
