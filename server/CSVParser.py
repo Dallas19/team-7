9468952dfc87450362c80d7e32a8659ac58bd452
joshuadargan <joshuadargan282@ufl.edu>
2019-10-19 11:35:56 -0400

    fixed parser

--- server/CSVParser.py
+++ server/CSVParser.py
@@ -9,38 +9,13 @@ class CSVParser:
     #Input:     path\\filename
     #Output:    2D list of strings representing the rows from the student CSV.
     @staticmethod
-    def parseStudentFile(filename):
+    def parseFile(filename):
         # initializing the titles and rows list
         fields = []
         rows = []
 
         cur_path = os.path.dirname(__file__)
         new_path = os.path.relpath(filename, cur_path)
-        # reading csv file
-        with open(new_path, 'r') as csvfile:
-            # creating a csv reader object
-            csvreader = csv.reader(csvfile)
-
-            # extracting field names through first row
-            fields = csvreader.__next__()
-
-            # extracting each data row one by one
-            for row in csvreader:
-                rows.append(row)
-        return rows
-
-    @staticmethod
-    #Input:     path\\filename (CSV file)
-    #Output:    2D list of strings representing the rows from the CSV.
-    def parseCompanyFile(filename):
-        # initializing the titles and rows list
-        fields = []
-        rows = []
-
-        cur_path = os.path.dirname(__file__)
-        new_path = os.path.relpath(filename, cur_path)
-
-
         # reading csv file
         with open(new_path, 'r') as csvfile:
             # creating a csv reader object
