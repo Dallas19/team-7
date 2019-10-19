06f73dc8969103440b8e2d102a3f637dde1b75a9
Mike Giang <mikegiang963@gmail.com>
2019-10-18 22:50:26 -0500

    Added drag and drop boxes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
+import Dropzone from "react-dropzone";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
+  const [studentFileName, setStudentFileName] = useState(
+    "Drag or Click to Browse File"
+  );
+  const [companyFileName, setCompanyFileName] = useState(
+    "Drag or Click to Browse File"
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <h4> Auto Matcher </h4>
+        <h1> Auto Matcher </h1>
+        <h4>Student Ranking File</h4>
+        <Dropzone
+          className="Dropzone"
+          onDrop={acceptedFiles => setStudentFileName(acceptedFiles[0].name)}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>{studentFileName}</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        <h4>Company Ranking File</h4>
+        <Dropzone
+          className="Dropzone"
+          onDrop={acceptedFiles => setCompanyFileName(acceptedFiles[0].name)}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>{companyFileName}</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
         <div>
           <Button className="Button"> Interviews </Button>
           <Button className="Button"> Offers </Button>
@@ -16,6 +52,6 @@ const App = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
