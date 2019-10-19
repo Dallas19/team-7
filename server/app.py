8f1ab1035db59c4c4a0690bb3c61e9fc7498e05c
tejaswi98 <tejaswi98@gmail.com>
2019-10-19 03:32:32 -0500

    Endpoint accepts two files and returns a csv file
    Endpoint accepts two files and returns a csv file

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS
+import os.path
+
 
 app = Flask(__name__)
 CORS(app)
@@ -7,14 +9,28 @@ CORS(app)
 @app.route('/getInterviewSchedule', methods=['POST'])
 def interviewScheduler():
     #file = request.files['StudentInput_Example']
-    files = request.files.getlist('files[]')
-    print(files)
-    return "success"
+    #files = request.files.getlist('files[]')
+    #print(files[0])
+    path = "server/testFiles/CompanyInput_Blank.csv"
+    return app.send_static_file(path)
+
 
 @app.route('/getOfferMatches')
 def offerMatches():
-  return 'OfferMatches'
+  path = "server/testFiles/CompanyInput_Blank.csv"
+  return app.send_static_file(path)
 
+def get_file(filename):  # pragma: no cover
+    try:
+        src = filename
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
 
 class File:
   def __init__(self, name):
